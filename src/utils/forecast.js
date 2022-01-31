eeb9c6c7476554cfae5a98130445e99f5e7f6a1d
Dhruvi Shah <dhruvishah549@gmail.com>
2022-01-30 23:54:54 -0500

    Add humidity percentage

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {    // will run if something like invalid location was provided
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature 
+            + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.' )
         }
     })
 }
